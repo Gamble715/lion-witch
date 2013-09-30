7fb9d6da4c157a3e940196e000986ff23785e3cd
MakerSquare Student <students@themakersquare.com>
2013-09-30 16:09:46 -0500

    Add a show method and json integration.

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -1,5 +1,15 @@
 class WardrobeItemsController < ApplicationController
   def index
     @wardrobe_items = WardrobeItem.all
+    respond_to do |format|
+      format.json { render json: @wardrobe_items}
+    end
+  end
+
+  def show
+    @wardrobe_item = WardrobeItem.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @wardrobe_item}
+    end
   end
 end
